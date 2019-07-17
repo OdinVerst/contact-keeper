816588835899bad9d725193562bebb745cd2ca9c
Sergey Razdolyansy <grafcorp01@gmail.com>
2019-07-17 16:29:41 +0300

    Add connet to DB and upd gitignore, add editorconfig

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = require("express");
+const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
-app.get("/", (res, req) => req.json({ msg: "Welcom to Contact Keeper" }));
+app.get('/', (res, req) => req.json({ msg: 'Welcom to Contact Keeper' }));
 
 //Define Routes
-app.use("/api/users", require("./routes/users"));
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/contacts", require("./routes/contacts"));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+//Connect DB
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server start on ${PORT} - port`);
+	console.log(`Server start on ${PORT} - port`);
 });
