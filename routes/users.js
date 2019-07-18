87cf8efe2aab564bb7efd8c151dab51165035f49
Sergey Razdolyansy <grafcorp01@gmail.com>
2019-07-18 17:38:56 +0300

    Add User model

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,28 @@
-const experss = require("express");
+const experss = require('express');
 const router = experss.Router();
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 //@route    POST api/users
 //@desc     Registr a user
 //@access   Public
-router.post("/", (res, req) => {
-  req.send(`Register user`);
-});
+router.post(
+	'/',
+	[
+		check('name', 'Name is requiered!')
+			.not()
+			.isEmpty(),
+		check('email', 'Email include valid email').isEmail(),
+		check('password', 'Please enter valid password').isLength({ min: 6 })
+	],
+	(req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		res.send(req.body);
+	}
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,15 +2,19 @@ const express = require('express');
 const connectDB = require('./config/db');
 
 const app = express();
-app.get('/', (res, req) => req.json({ msg: 'Welcom to Contact Keeper' }));
+
+//Connect DB
+connectDB();
+
+//Init middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.json({ msg: 'Welcom to Contact Keeper' }));
 
 //Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
-
-//Connect DB
-connectDB();
 
 const PORT = process.env.PORT || 5000;
 
