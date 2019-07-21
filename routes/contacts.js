6ded1cc56fa8748f2512bd9abf5a9bde208f4af3
Sergey Razdolyansy <grafcorp01@gmail.com>
2019-07-21 11:19:46 +0300

    Add new contact (req)

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,5 +1,6 @@
 const experss = require('express');
 const router = experss.Router();
+const { check, validationResult } = require('express-validator');
 
 const auth = require('../midlleware/auth');
 const User = require('../models/User');
@@ -23,9 +24,39 @@ router.get('/', auth, async (req, res) => {
 //@route    POST api/auth
 //@desc     Add new contact
 //@access   Private
-router.post('/', (res, req) => {
-	req.send(`Add new contact`);
-});
+router.post(
+	'/',
+	[
+		auth,
+		check('name', 'Name is required')
+			.not()
+			.isEmpty()
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { name, email, phone, type } = req.body;
+		try {
+			const newContact = new Contact({
+				name,
+				email,
+				phone,
+				type,
+				user: req.user.id
+			});
+
+			const contact = await newContact.save();
+
+			res.json(contact);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('500 Server Error!');
+		}
+	}
+);
 
 //@route    PUT api/auth/:id
 //@desc     Update contact
