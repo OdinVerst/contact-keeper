1d484c366534ae8a17ac37a7fbbeff5ae483033b
Sergey Razdolyansy <grafcorp01@gmail.com>
2019-07-23 19:31:47 +0300

    Clean file, add css and create Navbar

---
+++ client/src/component/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+	return (
+		<div>
+			<h1>About this App</h1>
+			<p className="my-1">This is a fullstack React app for keeping contact</p>
+			<p className="bg-dark p">
+				<b>Version: </b> 1.0.0
+			</p>
+		</div>
+	);
+};
+
+export default About;
