3cab62a4d6dfeffe45cac99547ac72c4225ebf85
Sergey Razdolyansy <grafcorp01@gmail.com>
2019-07-24 19:58:54 +0300

    Add context Contact and add types

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,56 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';
+
+import ContactContext from './contactContext';
+import contactReducer from './contactReduser';
+import {
+	ADD_CONTACT,
+	DELETE_CONTACT,
+	SET_CURRENT,
+	CLEAR_CURRENT,
+	UPDATE_CONTACT,
+	FILTER_CONTACT,
+	CLEAR_FILTER
+} from '../types';
+
+const ContactState = props => {
+	const initialState = {
+		contacts: [
+			{
+				id: 1,
+				name: 'Jhon Doe',
+				email: 'jhon@gmail.com',
+				phone: '444-444-444',
+				type: 'personal'
+			},
+			{
+				id: 2,
+				name: 'Brad Travis',
+				email: 'brad@gmail.com',
+				phone: '555-444-444',
+				type: 'professional'
+			},
+			{
+				id: 3,
+				name: 'Tom Hard',
+				email: 'tom@gmail.com',
+				phone: '444-222-444',
+				type: 'personal'
+			}
+		]
+	};
+
+	const [state, distpatch] = useReducer(contactReducer, initialState);
+
+	return (
+		<ContactContext.Provider
+			value={{
+				contacts: state.contacts
+			}}
+		>
+			{props.children}
+		</ContactContext.Provider>
+	);
+};
+
+export default ContactState;
